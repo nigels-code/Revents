bdcba82217e88c777af5430e8163dbe063db512d
Nigel <75135800+nigels-code@users.noreply.github.com>
2021-07-26 09:40:54 +0100

    Update firebase.js

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storgaeBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
 
